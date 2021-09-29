27827764bfc1c9de49b4b4de0978f654df336467
Decristony Araujo <79161227+decristony@users.noreply.github.com>
2021-09-29 16:43:03 -0300

    final dos exercicios

--- exercicios guanabara 04/script.js
+++ exercicios guanabara 04/script.js
@@ -3,28 +3,30 @@
 function tabuada() {
   var numero = document.getElementById('primeiro')
   var res = document.getElementById('select')
-  var contando= document.getElementById('res')
+  var contando = document.getElementById('res')
   var n = Number(numero.value)
-  
-  if(numero.value.length==0){
+
+
+
+  if (numero.value.length == 0) {
     alert('por favor digite um numero')
-  }else{
-    
+  } else {
+
     res.innerHTML = ""
     contando.innerHTML = `tabuada de ${n} gerada com sucesso`
-    
-    for(var c=1;c<=10;c++){
-      
-      var r = n*c
+
+    for (var c = 1; c <= 10; c++) {
+
+      var r = n * c
       console.log(r)
       let item = document.createElement('option')
-      item.text=`${n} x ${c} = ${r}`
-      item.value =`tab${c}`
+      item.text = `${n} x ${c} = ${r}`
+      item.value = `tab ${c}`
       //adiciona um elemento filho 
       res.appendChild(item)
-     
+
     }
-   
+
   }
 }
 
--- exercicios guanabara 05/script.js
+++ exercicios guanabara 05/script.js
@@ -1,30 +1,32 @@
+var num = document.getElementById('fnum').value
+var lista = document.getElementById('flista')
+var res = document.getElementById('resumo')
+let valores = []
 
 
-function tabuada() {
-  var numero = document.getElementById('primeiro')
-  var res = document.getElementById('select')
-  var contando= document.getElementById('res')
-  var n = Number(numero.value)
-  
-  if(numero.value.length==0){
-    alert('por favor digite um numero')
-  }else{
-    
-    res.innerHTML = ""
-    contando.innerHTML = `tabuada de ${n} gerada com sucesso`
-    
-    for(var c=1;c<=10;c++){
-      
-      var r = n*c
-      console.log(r)
-      let item = document.createElement('option')
-      item.text=`${n} x ${c} = ${r}`
-      item.value =`tab${c}`
-      //adiciona um elemento filho 
-      res.appendChild(item)
-     
-    }
-   
+function isNumero(n) {
+  if (Number(n) >= 1 && Number(n) <= 100) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function inLista(n, l) {
+  if (l.indexOf(Number(n)) != -1) {
+    return true
+  } else {
+    return false
+  }
+}
+function adicionar() {
+  if (isNumero(num) && !inLista(num, valores)) {
+    valores.push(Number(num))
+    let item = document.createElement('option')
+    item.text = `valor ${num} adicionado`
+    lista.appendChild(item)
+  } else {
+    alert('valor invalido ou ja consta na lista')
   }
 }
 
